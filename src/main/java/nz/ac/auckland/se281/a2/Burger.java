6fab961b903b93e8c6149cdec868e5b72f35ddbd
lia-arroyo <liayzabel@gmail.com>
2022-04-18 15:22:36 +1200

    created a Burger class that extends Item

---
+++ src/main/java/nz/ac/auckland/se281/a2/Burger.java
@@ -0,0 +1,11 @@
+// declaring package
+package nz.ac.auckland.se281.a2;
+
+// class declaration
+public class Burger extends Item {
+
+	public Burger(String name, float price) {
+		super(name, price);
+	}
+
+}
